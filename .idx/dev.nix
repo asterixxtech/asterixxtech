b7fa65dd4f43d8932d252832a0762c2c47c23e32
noeldycreator <noeldycreator@gmail.com>
2024-06-06 15:17:15 +0000

    Nix configuration on IDX

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,7 +9,7 @@
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
+    pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
 
@@ -18,23 +18,26 @@
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
-      # "vscodevim.vim"
+       "vscodevim.vim"
+       "astro-build.astro-vscode"
+       "bradlc.vscode-tailwindcss"
+       "esbenp.prettier-vscode"
     ];
 
     # Enable previews
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+         web = {
+           # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+           # and show it in IDX's web preview panel
+           command = ["npm" "run" "dev"];
+           manager = "web";
+           env = {
+             # Environment variables to set for your server
+             PORT = "$PORT";
+           };
+         };
       };
     };
 
@@ -43,7 +46,7 @@
       # Runs when a workspace is first created
       onCreate = {
         # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
+        npm-install = "npm install";
       };
       # Runs when the workspace is (re)started
       onStart = {
